dceefbd1224680e699c8657becec603fa594c150
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-10-14 14:09:33 +0200

    feat: fetching products

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,6 +1,5 @@
-const API_PATH = "https://api.airtable.com/v0/appikgzY018mp2gcs";
-const API_TOKEN =
-  "pat5g1IXZYHvSxx0o.7181c416fc88c8ee5cbb673e8c43da65009e9044b1c7bb0d2e32ff9110ae7467";
+const API_PATH = process.env.NEXT_PUBLIC_API_PATH;
+const API_TOKEN = process.env.NEXT_PUBLIC_API_TOKEN;
 
 class InvalidResponse extends Error {}
 
