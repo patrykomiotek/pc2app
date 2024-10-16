6cdb4a0f6d402ce94ed72309ca51865efba1d8a8
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-10-16 10:37:47 +0200

    feat: placeholders for rate limiter

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -1,5 +1,7 @@
 import { NextRequest } from "next/server";
-import rateLimit from "express-rate-limit";
+// import rateLimit from "express-rate-limit";
+
+const rateLimitMap = new Map();
 
 export const rateLimiter = (req: NextRequest) => {
   const requestHeaders = new Headers(req.headers);
@@ -7,12 +9,29 @@ export const rateLimiter = (req: NextRequest) => {
   const limit = 5; // Limiting requests to 5 per minute per IP
   const windowMs = 60 * 1000; // 1 minute
 
-  // if (!rateLimitMap.has(ip)) {
-  //     rateLimitMap.set(ip, {
-  //         count: 0,
-  //         lastReset: Date.now(),
-  //     });
-  // }
+  if (!rateLimitMap.has(ip)) {
+    rateLimitMap.set(ip, {
+      count: 0,
+      lastReset: Date.now(),
+    });
+  }
+
+  const ipData = rateLimitMap.get("ip");
+  if (Date.now() - ipData.lastReset > windowMs) {
+    ipData.count = 0;
+    ipData.lastReset = Date.now();
+  }
+
+  if (ipData.count >= limit) {
+    // new Promise || response.status(404).statusText('too many requests')
+    // TODO: return promise or response
+    // return
+  }
+
+  ipData.count += 1;
+
+  // TODO: return resolved promise
+
   return new Promise((resolve) => resolve({ status: "ok" }));
 };
 
